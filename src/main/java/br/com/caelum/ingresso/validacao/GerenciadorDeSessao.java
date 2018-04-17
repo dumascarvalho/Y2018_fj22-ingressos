bd37c5f6d2c56a1e80d91c28610457d931402ed8
Dumas Carvalho <dumas.c@samsung.com>
2018-04-17 15:52:10 -0300

    inclusão de validações

---
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -0,0 +1,39 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessao {
+	
+	private List<Sessao> sessoesDaSala;
+	
+	public GerenciadorDeSessao(List<Sessao> sessoesDaSala) {
+		this.sessoesDaSala = sessoesDaSala;
+	}
+	
+	private boolean horarioIsConflitante(Sessao sessaoExistente, Sessao sessaoNova) {
+		
+		LocalDate hoje = LocalDate.now();
+		
+		LocalDateTime horarioSessaoNova = sessaoNova.getHorario().atDate(hoje);
+		LocalDateTime terminoSessaoNova = sessaoNova.getHorarioTermino().atDate(LocalDate.now());
+		LocalDateTime horarioSessaoExistente = sessaoExistente.getHorario().atDate(hoje);
+		LocalDateTime terminoSessaoExistente = sessaoExistente.getHorarioTermino().atDate(LocalDate.now());
+		
+		boolean terminaAntes = terminoSessaoNova.isBefore(horarioSessaoExistente);
+		boolean comecaDepois = horarioSessaoNova.isAfter(terminoSessaoExistente);
+		
+		if (terminaAntes || comecaDepois) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean cabe(Sessao sessaoNova) {
+		
+		return sessoesDaSala.stream().noneMatch(sessaoExistente -> horarioIsConflitante(sessaoExistente, sessaoNova));
+	}
+}
